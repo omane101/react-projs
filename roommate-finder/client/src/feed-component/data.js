3b2a5122a2a847c7d2ce9faf14de85d17dcd4cfc
Mohamed Omane <omanem@union.edu>
2021-03-12 22:12:16 -0500

    init

--- roommate-finder/client/src/feed-component/data.js
+++
@@ -1,9 +0,0 @@
-// sample data file 
-
-export const data = [
-    { text: "Good morning! ", name:"Jake" },
-    { text: "Happy Friday Everyone - ", name:"Louisa" },
-    { text: "New to the area - ", name:"Zhang" },
-];
-
-export default data;
